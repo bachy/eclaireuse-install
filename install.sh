ad5b6adc9f550c02f32ec5a8c816930d0f0a8066
Bachir Soussi Chiadmi <bachir@g-u-i.me>
2015-12-07 15:21:59 +0100

    lightdm automogin

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 #bachir soussi chiadmi
+#
+# run this script as root
+#
+
+# get the current position
+_cwd="$(pwd)"
 
 # Install a text editor, because it’s always usefull
 apt-get install vim
@@ -10,3 +16,7 @@ apt-get install vim
 # - matchbox-window-manager: [Window manager](https://www.yoctoproject.org/tools-resources/projects/matchbox)
 # - chromium: [Web browser](https://wiki.debian.org/Chromium)
 apt-get install xorg lightdm matchbox-window-manager chromium open-ssh
+
+# configure lightdm (autologin)
+mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.ORIGINAL
+cp $_cwd/assets/lightdm.conf /etc/lightdm/
