be7d3708542af265fbf67a93e52d3b1c1399b0a1
Julien Gargot <julien@g-u-i.me>
2015-12-07 14:35:56 +0100

    Add comments to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 #bachir soussi chiadmi
 
+# Install a text editor, because it’s always usefull
 apt-get install vim
 
-apt-get install xorg lightdm matchbox-window-manager chromium
-
+# Install main environment
+# - xorg: [X Window server](https://wiki.debian.org/Xorg)
+# - lightdm: [Display Manager](https://wiki.debian.org/LightDM)
+# - matchbox-window-manager: [Window manager](https://www.yoctoproject.org/tools-resources/projects/matchbox)
+# - chromium: [Web browser](https://wiki.debian.org/Chromium)
+apt-get install xorg lightdm matchbox-window-manager chromium open-ssh
